36702ec10b0754b8ab5576e82fcfcaf1c782bd82
Vanden Herrewegen Guillaume <guillaume-vdh@skynet.be>
2020-03-09 21:05:16 +0100

    update

--- go-fullstack/backend/app.js
+++ go-fullstack/backend/app.js
@@ -2,19 +2,30 @@
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
         {
-            _id: '1',
-            date: '21/02/1552',
-            heure: '20:00',
-            participants: 'Pierre, Jacques',
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
         },
         {
-            _id: '2',
-            date: '24/04/1552',
-            heure: '10:00',
-            participants: 'Julien, Loic',
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
         },
     ];
     res.status(200).json(stuff);
