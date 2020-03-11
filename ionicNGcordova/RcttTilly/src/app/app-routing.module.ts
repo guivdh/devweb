98680cab9aa11ffea3274d105647b90d92c8f479
qlebrun <qnlebrun@hotmail.com>
2020-03-11 08:31:02 +0100

    add ionic/cordova/angular frontend structure

---
+++ ionicNGcordova/RcttTilly/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'open-app',
+    loadChildren: () => import('./pages/open-app/open-app.module').then( m => m.OpenAppPageModule)
+  },
+  {
+    path: 'connection',
+    loadChildren: () => import('./pages/connection/connection.module').then( m => m.ConnectionPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ ionicNGcordova/RcttTilly/src/app/pages/open-app/open-app.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-open-app',
+  templateUrl: './open-app.page.html',
+  styleUrls: ['./open-app.page.scss'],
+})
+export class OpenAppPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
