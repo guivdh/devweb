ec88b737d80d52e56efe2a49e40bc7376c71556b
rebald <43856164+rebald@users.noreply.github.com>
2020-03-14 17:02:45 +0100

    register fom+validation form

--- fullstack/ionicNGcordova/RcttTilly/src/app/pages/register/register.page.ts
+++ fullstack/ionicNGcordova/RcttTilly/src/app/pages/register/register.page.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators,FormControl } from "@angular/forms";
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup;
 
-  ngOnInit() {
+  constructor(public formBuilder: FormBuilder) { 
+    this.registerForm = this.formBuilder.group({
+      Nom: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z- ]+$')
+      ])),
+      Prenom: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z- ]+$')
+      ])),
+      Matricule: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[0-9]+$'),
+        Validators.maxLength(6),
+        Validators.minLength(6)
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+        Validators.minLength(8)
+      ])),
+      mail: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ]))
+    });
+  }
+
+  onClickSubmit() {
+    console.log('Start login with: ' 
+                + this.registerForm.value.mail + ':' 
+                + this.registerForm.value.password);
+  }
+
+  ngOnInit(): void {
   }
 
 }
