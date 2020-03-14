38b43ad1493ce1a8cad24c8f2f6f6a6dcfee83d5
qlebrun <qnlebrun@hotmail.com>
2020-03-14 14:31:58 +0100

    add routerlink in first pages

--- fullstack/ionicNGcordova/RcttTilly/src/app/app-routing.module.ts
+++ fullstack/ionicNGcordova/RcttTilly/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'open-app', pathMatch: 'full' },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
