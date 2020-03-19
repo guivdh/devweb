fa962f0f3d48339b19311bfecfa26bd1116e0457
qlebrun <qnlebrun@hotmail.com>
2020-03-19 21:03:01 +0100

    add user service for test

---
+++ fullstack/ionicNGcordova/RcttTilly/src/app/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core'
+import {AngularFireAuth} from '@angular/fire/auth'
+
+
+interface user {
+
+    username: string,
+    uid: string
+}
+
+@Injectable()
+export class UserService{
+
+    private user: user 
+    constructor(private afAuth: AngularFireAuth){
+
+
+
+    }
+
+
+    setUser(user: user){
+
+    this.user = user
+
+    }
+
+
+    getUID(){
+        if(!this.user){
+
+            if(this.afAuth.auth.currentUser)
+            {
+                const user = this.afAuth.auth.currentUser;
+                this.setUser({
+                    username: user.email,
+                    uid: user.uid
+                });
+                return user.uid;
+            } 
+            else{
+                throw new Error("Utilisateur non connecté!")
+            } 
+        }   
+        
+        else{
+            return this.user.uid;
+        }
+    }
+}
