f5e8fb9678decfa1e8ea03aae9b9cf975e53dc11
Vanden Herrewegen Guillaume <guillaume-vdh@skynet.be>
2020-03-22 16:13:18 +0100

    update

--- fullstack/backend/app.js
+++ fullstack/backend/app.js
@@ -42,6 +42,12 @@ app.put('api/stuff/:id', (req, res, next) => {
         .catch(error => res.status(404).json({ error }));
 });
 
+app.delete('api/stuff/:id', (req, res, next) => {
+    Thing.delete({_id: req.params.id})
+        .then(() => res.status(200).json({message: 'Objet supprimé !'}))
+        .catch(error  => res.status(400).json({error}));
+})
+
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
         .then(thing => res.status(200).json(thing))
