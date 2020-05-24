00ad8ba9080a04d8ff88e8e89a6d8086d1ae9ebb
qlebrun <qnlebrun@hotmail.com>
2020-05-24 19:38:48 +0200

    add functions for directory data in api service

--- fullstack/ionicNGcordova/RcttTilly/src/app/services/api/api.service.ts
+++ fullstack/ionicNGcordova/RcttTilly/src/app/services/api/api.service.ts
@@ -1,126 +1,73 @@
 import { Injectable } from '@angular/core';
 
-import { Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { catchError, tap, map } from 'rxjs/operators';
-import { EventRandom } from '../../event';
-import { UserRandom } from '../../user';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 
-const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
-const apiUrlEvent = 'http://localhost:3000/evenement';
+import 'firebase/firestore'; 
+import { UserService } from '../user/user.service';
+import { Router } from '@angular/router';
 
-const apiUrlUser = 'http://localhost:3000/utilisateur';
 
+
+export class Responsible {
+  Firstname: string;
+  Name: string;
+  Num: number;
+  Role:string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+  private dbPath = 'users';
+
+  responsiblesRef: AngularFirestoreCollection<Responsible> = null;
+
+  public responsibles: Responsible[];
 
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error); // log to console instead
-      return of(result as T);
-    };
-  }
+
+  constructor(   
+    private afStr: AngularFirestore,
+    private router: Router
+     ) { 
+      this.responsiblesRef = afStr.collection(this.dbPath);
+
+     }
 
 
-  //User CRUD
+  public getResponsibleById(id): AngularFirestoreDocument<Responsible>{
+    //alert("je suis "+id);
+    
+    return this.responsiblesRef.doc(id);
 
-  getUsers(): Observable<UserRandom[]> {
-    return this.http.get<UserRandom[]>(apiUrlUser)
-      .pipe(
-        tap(UserRandom => console.log('fetched Users')),
-        catchError(this.handleError('getUsers', []))
-      );
-  }
-  
-  getUser(id: any): Observable<UserRandom> {
-    const url = `${apiUrlUser}/${id}`;
-    return this.http.get<UserRandom>(url).pipe(
-      tap(_ => console.log(`fetched User id=${id}`)),
-      catchError(this.handleError<UserRandom>(`getUser id=${id}`))
-    );
-  }
-  
-  addUser(user: UserRandom): Observable<UserRandom> {
-    return this.http.post<UserRandom>(apiUrlUser, user, httpOptions).pipe(
-      tap((prod: UserRandom) => console.log(`added User w/ id=${prod.matricule}`)),
-      catchError(this.handleError<UserRandom>('addUser'))
-    );
-  }
-  
-  updateUser(id: any, user: any): Observable<any> {
-    const url = `${apiUrlUser}/${id}`;
-    return this.http.put(url, user, httpOptions).pipe(
-      tap(_ => console.log(`updated UserRandom id=${id}`)),
-      catchError(this.handleError<any>('updateUser'))
-    );
-  }
-  
-  deleteUser(id: any): Observable<UserRandom> {
-    const url = `${apiUrlUser}/${id}`;
-  
-    return this.http.delete<UserRandom>(url, httpOptions).pipe(
-      tap(_ => console.log(`deleted User id=${id}`)),
-      catchError(this.handleError<UserRandom>('deleteUser'))
-    );
   }
 
 
 
 
+  
+  public getResponsibles(): AngularFirestoreCollection<Responsible> {
 
-  //Event CRUD
-  getEvents(): Observable<EventRandom[]> {
-    return this.http.get<EventRandom[]>(apiUrlEvent)
-      .pipe(
-        tap(EventRandom => console.log('fetched Events')),
-        catchError(this.handleError('getEvents', []))
-      );
+    return this.responsiblesRef;
+
+   }
+
+   public createResponsible(responsible: Responsible): void {
+    this.responsiblesRef.add({...responsible});
   }
-  
-  getEvent(id: any): Observable<EventRandom> {
-    const url = `${apiUrlEvent}/${id}`;
-    return this.http.get<EventRandom>(url).pipe(
-      tap(_ => console.log(`fetched Event id=${id}`)),
-      catchError(this.handleError<EventRandom>(`getEvent id=${id}`))
-    );
+
+  updateResponsible(id: string, value: any): Promise<void> {
+      
+    return this.responsiblesRef.doc(id).update(value);
+
   }
-  
-  addEvent(event: EventRandom): Observable<EventRandom> {
-    return this.http.post<EventRandom>(apiUrlEvent, event, httpOptions).pipe(
-      tap((prod: EventRandom) => console.log(`added Event w/ id=${prod._id}`)),
-      catchError(this.handleError<EventRandom>('addEvent'))
-    );
-  }
-  
-  updateEvent(id: any, event: any): Observable<any> {
-    const url = `${apiUrlEvent}/${id}`;
-    return this.http.put(url, event, httpOptions).pipe(
-      tap(_ => console.log(`updated EventRandom id=${id}`)),
-      catchError(this.handleError<any>('updateEvent'))
-    );
-  }
-  
-  deleteEvent(id: any): Observable<EventRandom> {
-    const url = `${apiUrlEvent}/${id}`;
-  
-    return this.http.delete<EventRandom>(url, httpOptions).pipe(
-      tap(_ => console.log(`deleted Event id=${id}`)),
-      catchError(this.handleError<EventRandom>('deleteEvent'))
-    );
+
+  deleteResponsible(id: string){
+    return this.responsiblesRef.doc(id).delete();
+
   }
 
 
-
-
-
-
-  
 }
