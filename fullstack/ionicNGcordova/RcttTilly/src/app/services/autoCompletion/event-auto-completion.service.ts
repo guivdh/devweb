8ae66eb1d7623b2a889d55ff10a385d2fc445718
qlebrun <qnlebrun@hotmail.com>
2020-05-26 01:50:06 +0200

    add getRole function

--- fullstack/ionicNGcordova/RcttTilly/src/app/services/autoCompletion/event-auto-completion.service.ts
+++ fullstack/ionicNGcordova/RcttTilly/src/app/services/autoCompletion/event-auto-completion.service.ts
@@ -7,7 +7,10 @@ export interface TitleType {
   id: number;
   name: string;
 }
-
+export interface Role {
+  id: number;
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -15,63 +18,7 @@ export interface TitleType {
 export class EventAutoCompletionService {
 
   eventTitle=[];
-
-
-  public titles: TitleType[] = [
-    {
-      id: 1,
-      name: 'Match - Division2A_N',
-    },
-    {
-      id: 2,
-      name: 'Match - Division1B_P',
-    },
-    {
-      id: 3,
-      name: 'Match - Division3C_P',
-    },
-    {
-      id: 4,
-      name: 'Match - Division3D_P',
-    },
-    {
-      id: 5,
-      name: 'Match - Division4E_P',
-    },
-    {
-      id: 6,
-      name: 'Match - Division5F_P',
-
-    },
-    {
-      id: 7,
-      name: 'Match - Division6G_P',
-    },
-    {
-      id: 8,
-      name: 'Match - Division7_debutant',
-    },
-    {
-      id: 9,
-      name: 'Entrainement libre',
-    },
-    {
-      id: 10,
-      name: 'Entrainement dirigé',
-    },
-    {
-      id: 11,
-      name: 'Souper du club',
-    },
-    {
-      id: 12,
-      name: 'Réunion - capitaine',
-    },
-    {
-      id: 13,
-      name: 'Réunion - responsable',
-    }
-  ];
+  roleUser=[];
 
 
   constructor(   
@@ -81,21 +28,6 @@ export class EventAutoCompletionService {
     }
   
 
-/*   public getEventTitles() {
-
-
-    this.afDB.list('Event').snapshotChanges(['child_added']).subscribe(actions => {
-         this.eventTitle = [];
-    
-         actions.forEach(action => {   
-           this.eventTitle.push({
-            _id:action.payload.key,
-            title: action.payload.exportVal().title,
-           });
-         });
-         return this.eventTitle;
-       });    
-     } */
 
 
 
@@ -122,4 +54,22 @@ export class EventAutoCompletionService {
        }
 
 
+
+       public getRole(): Role[] {
+        this.roleUser=[];
+      this.afDB.list('Role').snapshotChanges(['child_added']).subscribe(actions => {
+           actions.forEach(action => {
+             //console.log('action: ' + action.payload.key);
+
+             this.roleUser.push({
+               id: 1,
+               name: action.payload.exportVal().name
+             });
+         
+           });
+
+         });
+
+         return this.roleUser;
+       }
 }
